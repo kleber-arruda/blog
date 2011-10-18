74e64b5e6a3f00bad9cc9c4f78f2853f7f27581e
Kleber <kleber@kleber-linux.(none)>
2011-10-18 00:21:54 -0200

    rota default

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Blog::Application.routes.draw do
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -50,7 +50,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
