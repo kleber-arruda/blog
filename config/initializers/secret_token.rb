140cb813cf3cf3ece18382f75ac21774e1bd9a67
Kleber <kleber@kleber-linux.(none)>
2011-10-17 23:40:53 -0200

    blog app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7b4e70d7641a77b1c72a4712bc7ca8f0ff23287e46c56744c1821962311a16a6a2eaa51a9a7fe3e03ae6f42c9f1cbd77258d8d66d09f74377f07e921a434b2a3'
